6f42fe4938f9802d7f0c481d7bd7f77327175621
celsojr-websters <celso.j@websters.ae>
2023-10-26 17:32:42 +0400

    raylib

--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,43 @@
-Console.WriteLine("Hello, Browser!");
+namespace DotnetWasm;
 
-[SupportedOSPlatform("browser")]
-public partial class Program
+public static class Program
 {
-    [JSExport]
-    internal static string Greeting()
+    public static int Main()
     {
-        string text = $"Hello, World! Greetings from {GetHRef()}";
-        Console.WriteLine(text);
-        Warn("This is a warn!");
-        Alert("This is an alert!");
-        return text;
+        // Initialization
+            //--------------------------------------------------------------------------------------
+            const int screenWidth = 800;
+            const int screenHeight = 450;
+
+            InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+            SetTargetFPS(60);
+            //--------------------------------------------------------------------------------------
+
+            // Main game loop
+            while (!WindowShouldClose())    // Detect window close button or ESC key
+            {
+                // Update
+                //----------------------------------------------------------------------------------
+                // TODO: Update your variables here
+                //----------------------------------------------------------------------------------
+
+                // Draw
+                //----------------------------------------------------------------------------------
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+
+                DrawText("Hey dev :)", 350, 200, 20, MAROON);
+
+                EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+
+            // De-Initialization
+            //--------------------------------------------------------------------------------------
+            CloseWindow();        // Close window and OpenGL context
+            //--------------------------------------------------------------------------------------
+
+            return 0;
     }
 }
